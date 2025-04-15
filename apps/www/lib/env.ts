4a66720ff4c22878a1ceb3682f43e80ad383e1b3
James Perkins <james@unkey.dev>
2025-04-15 09:00:24 -0400

    Remove the old stats from this project

--- apps/www/lib/env.ts
+++ apps/www/lib/env.ts
@@ -6,12 +6,3 @@ export const env = () =>
       NEXT_PUBLIC_BASE_URL: z.string().url().default("https://unkey.com"),
     })
     .parse(process.env);
-
-export const dbEnv = () =>
-  z
-    .object({
-      DATABASE_HOST: z.string(),
-      DATABASE_USERNAME: z.string(),
-      DATABASE_PASSWORD: z.string(),
-    })
-    .parse(process.env);
