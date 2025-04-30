cfa8910268bea734009f858f03ca0fa82837470b
Richard Poelderl <richard.poelderl@gmail.com>
2025-04-30 17:59:21 +0200

    database fix

--- apps/generator /drizzle.config.ts
+++ apps/generator /drizzle.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
+console.log(`the env vars: ${process.env.MARKETING_DATABASE_HOST} ${process.env.MARKETING_DATABASE_USERNAME} ${process.env.MARKETING_DATABASE_PASSWORD?.slice(0, 4)}***** ${process.env.MARKETING_DATABASE_NAME}`);
+
 export default defineConfig({
   verbose: true,
   schema: "./src/lib/db-marketing/schemas/*.ts",
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
-    host: process.env.MARKETING_DATABASE_HOST!,
-    user: process.env.MARKETING_DATABASE_USERNAME!,
-    password: process.env.MARKETING_DATABASE_PASSWORD!,
-    database: process.env.MARKETING_DATABASE_NAME!,
-    ssl: {
-      rejectUnauthorized: true,
-    },
+    url: `mysql://${process.env.MARKETING_DATABASE_USERNAME}:${process.env.MARKETING_DATABASE_PASSWORD}@${process.env.MARKETING_DATABASE_HOST}/marketing`,
   },
 });
--- apps/generator /src/lib/db-marketing/schemas/keywords.ts
+++ apps/generator /src/lib/db-marketing/schemas/keywords.ts
@@ -12,20 +12,20 @@ export const keywords = mysqlTable(
     id: int("id").primaryKey().autoincrement(),
     inputTerm: varchar("input_term", { length: 767 }).notNull(),
     keyword: varchar("keyword", { length: 767 }).notNull(),
-    inputTermAndKeywordHash: varchar("input_term_and_keyword_hash", { length: 64 }).notNull(), // so that we avoid having to halve the inputTerm/keywords length
+    inputTermAndKeywordHash: varchar("input_term_and_keyword_hash", { length: 64 }), // so that we avoid having to halve the inputTerm/keywords length
     source: varchar("source", { length: 767 }).notNull(),
     sourceUrl: varchar("source_url", { length: 767 }),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
   },
-  (table) => ({
-    inputTermIdx: index("input_term_idx").on(table.inputTerm),
-    keywordIdx: index("keyword_idx").on(table.keyword),
-    sourceUrlIdx: index("source_url_idx").on(table.sourceUrl),
-    uniqueKeyword: unique("keywords_input_term_keyword_hash_unique").on(
+  (table) => ([
+    index("input_term_idx").on(table.inputTerm),
+    index("keyword_idx").on(table.keyword),
+    index("source_url_idx").on(table.sourceUrl),
+    unique("keywords_input_term_keyword_hash_unique").on(
       table.inputTermAndKeywordHash,
     ),
-  }),
+  ]),
 );
 
 export const insertKeywordsSchema = createInsertSchema(keywords).extend({}).omit({ id: true });
