e13a643c9734d8932405d35b088ec71efc30ac5c
Richard Poelderl <richard.poelderl@gmail.com>
2025-04-30 18:23:02 +0200

    remove log

--- apps/generator /drizzle.config.ts
+++ apps/generator /drizzle.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "drizzle-kit";
 
-console.log(`the env vars: ${process.env.MARKETING_DATABASE_HOST} ${process.env.MARKETING_DATABASE_USERNAME} ${process.env.MARKETING_DATABASE_PASSWORD?.slice(0, 4)}***** ${process.env.MARKETING_DATABASE_NAME}`);
-
 export default defineConfig({
   verbose: true,
   schema: "./src/lib/db-marketing/schemas/*.ts",
