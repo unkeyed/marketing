f25e01494a2a7f83cad784afc2a1670d62cb34f6
James Perkins <jamesperkins@hey.com>
2025-05-04 13:44:47 -0400

    remove period

--- apps/www/app/hero/hero-main-section.tsx
+++ apps/www/app/hero/hero-main-section.tsx
@@ -7,7 +7,7 @@ export function HeroMainSection() {
   return (
     <div className="relative flex flex-col items-center text-center ">
       <h1 className="bg-gradient-to-br text-pretty text-transparent bg-gradient-stop bg-clip-text from-white via-white via-30% to-white/30  font-medium text-[32px] leading-none sm:text-[64px] xl:text-[82px] tracking-tighter">
-        Modern APIs <br /> Effortless Development.
+        Modern APIs <br /> Effortless Development
       </h1>
 
       <p className="mt-6 sm:mt-8 bg-gradient-to-br text-transparent text-balance bg-gradient-stop bg-clip-text max-w-sm sm:max-w-lg xl:max-w-4xl from-white/70 via-white/70 via-40% to-white/30 text-sm sm:text-[20px]">
