73001cc8304ae93c13e06ffa7d321cb7072be249
James Perkins <james@unkey.dev>
2025-10-23 14:16:29 -0700

    fix pages

--- apps/www/app/careers/[slug]/page.tsx
+++ apps/www/app/careers/[slug]/page.tsx
@@ -19,11 +19,9 @@ type Props = {
 };
 
 export async function generateStaticParams() {
-  return allCareers
-    .filter((c) => c.visible)
-    .map(({ slug }) => ({
-      slug,
-    }));
+  return allCareers.map(({ slug }) => ({
+    slug,
+  }));
 }
 export default async function Templates(props: Props) {
   const params = await props.params;
